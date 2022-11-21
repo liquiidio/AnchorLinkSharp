bb0a9521725af048d78d2eb155867d80876ca281
corvinmadh <grizzlycorv@googlemail.com>
2022-11-22 00:59:02 +0100

    changing to PlayerPrefsStorage

--- Examples/MultiUi/LoginExample.cs
+++ Examples/MultiUi/LoginExample.cs
@@ -32,7 +32,7 @@ public class LoginExample : MonoBehaviour
             ChainId = "1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4",
             Rpc = "https://api.wax.liquidstudios.io",
             ZlibProvider = new NetZlibProvider(),
-            Storage = new JsonLocalStorage()
+            Storage = new PlayerPrefsStorage()
         });
     }
 

5feccb9fca0cd6f557c28672af7e711c3bba5bcb
corvinmadh <grizzlycorv@googlemail.com>
2022-11-22 00:13:24 +0100

    changed RPC-address

--- Examples/MultiUi/LoginExample.cs
+++ Examples/MultiUi/LoginExample.cs
@@ -32,7 +32,11 @@ public class LoginExample : MonoBehaviour
             ChainId = "1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4",
             Rpc = "https://api.wax.liquidstudios.io",
             ZlibProvider = new NetZlibProvider(),
+<<<<<<< HEAD
             Storage = new PlayerPrefsStorage()
+=======
+            Storage = new JsonLocalStorage()
+>>>>>>> 80967f8 (changed RPC-address)
         });
     }
 

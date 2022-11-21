f4a0c878a2f6681a1777377d7a5a0f67191a9b3c
corvinmadh <grizzlycorv@googlemail.com>
2022-11-21 19:53:49 +0100

    removing System.Threading; usings

--- Src/Transports/Canvas/UnityCanvasTransport.cs
+++ Src/Transports/Canvas/UnityCanvasTransport.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Linq;
 using AnchorLinkSharp;
-using Assets.Packages.AnchorLinkTransportSharp.Src.Transports.UiToolkit;
 using EosioSigningRequest;
 using TMPro;
 using UnityEngine;

80967f85825590f36f4889202010e3ece05448eb
corvinmadh <grizzlycorv@googlemail.com>
2022-11-22 00:13:24 +0100

    changed RPC-address

--- Examples/MultiUi/LoginExample.cs
+++ Examples/MultiUi/LoginExample.cs
@@ -29,16 +29,10 @@ public class LoginExample : MonoBehaviour
         _link = new AnchorLink(new LinkOptions
         {
             Transport = Transport,
-            //ChainId = "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906", 
-            //Rpc = "https://eos.greymass.com",
             ChainId = "1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4",
-            Rpc = "https://wax.greymass.com",
+            Rpc = "https://api.wax.liquidstudios.io",
             ZlibProvider = new NetZlibProvider(),
             Storage = new JsonLocalStorage()
-            //chains: [{
-            //    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-            //    nodeUrl: 'https://eos.greymass.com',
-            //}]
         });
     }
 
